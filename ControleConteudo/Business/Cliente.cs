61117999daac1b065a1e8c51045f2b3732291ca5
SMARAPD - Alessandro Tulioli <ATULIOLI@SMARAPD.COM.BR>
2021-02-10 17:03:07 -0300

    Iniciado o projeto em 3 a camadas: Database, Business e Aplicação

---
+++ ControleConteudo/Business/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Business
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+
+        public List<Cliente> Clientes(string cliente)
+        {
+            var clientes = new List<Cliente>();
+            clientes.Add(new Cliente() { Nome = "Alessandro" });
+            return clientes;
+        }
+    }
+}
