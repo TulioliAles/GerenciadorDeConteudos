5af09179bb9e22562b6d6d7afda17b7d4f9e0711
SMARAPD - Alessandro Tulioli <ATULIOLI@SMARAPD.COM.BR>
2021-02-10 17:31:06 -0300

    Criadas Rotas para páginas Sobre e Contatos

--- ControleConteudo/ControleConteudo/App_Start/RouteConfig.cs
+++ ControleConteudo/ControleConteudo/App_Start/RouteConfig.cs
@@ -14,6 +14,18 @@ namespace ControleConteudo
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "sobre", 
+                "sobre",
+                new { controller = "Home", action = "About"}
+                );
+
+            routes.MapRoute(
+                "contato",
+                "contato",
+                new { controller = "Home", action = "Contact" }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
