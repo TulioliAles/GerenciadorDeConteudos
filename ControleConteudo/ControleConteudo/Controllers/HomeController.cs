33a4d3a0684b38bd6b989481a2684e1407692c48
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-02-11 22:32:37 -0300

    Criado o Model para Business e Database

--- ControleConteudo/ControleConteudo/Controllers/HomeController.cs
+++ ControleConteudo/ControleConteudo/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Business;
 using System.Web.Mvc;
 
 namespace ControleConteudo.Controllers
@@ -10,6 +7,8 @@ namespace ControleConteudo.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Paginas = new Pagina().Lista();
+
             return View();
         }
 
---
+++ ControleConteudo/Database/Pagina.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Database
+{
+    public class Pagina
+    {
+        private string sqlConn()
+        {
+            return ConfigurationManager.AppSettings["sqlConn"];
+        }
+
+        public DataTable Lista()
+        {
+            using (SqlConnection conection = new SqlConnection(sqlConn()))
+            {
+                string queryString = "select * from paginas";
+                SqlCommand command = new SqlCommand(queryString, conection);
+                command.Connection.Open();
+
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = command;
+
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                return table;
+            }
+        }
+    }
+}
